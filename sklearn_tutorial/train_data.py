8d987385b15af766b06f7b7169ef13bd74bda8fa
tayyabkhan00 <iron29700@gmail.com>
2025-12-06 15:37:35 +0530

    train data

---
+++ sklearn_tutorial/train_data.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+print("X_train:", X_train.shape)
+print("X_test:", X_test.shape)
+print("y_train:", y_train.shape)
+print("y_test:", y_test.shape)
