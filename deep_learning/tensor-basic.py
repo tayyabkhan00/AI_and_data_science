fd9c62459ac83fc910804cb2036b752d24ef4588
tayyabkhan00 <iron29700@gmail.com>
2025-12-11 02:34:30 +0530

    GPU Tensors

---
+++ deep_learning/tensor-basic.py
@@ -0,0 +1,15 @@
+import torch
+
+x = torch.tensor([1, 2, 3])        # 1D tensor
+m = torch.tensor([[1, 2], [3, 4]]) # 2D tensor
+
+a = torch.tensor([1., 2., 3.])
+b = torch.tensor([4., 5., 6.])
+
+print(a + b)
+print(a * b)
+print(a @ b)   # dot product
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+x = torch.tensor([1, 2, 3]).to(device)
+print(x.device)
