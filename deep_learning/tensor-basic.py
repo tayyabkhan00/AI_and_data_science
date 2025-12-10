c2d272fff9883a9c7762f30c6a63b88c9ff33a31
tayyabkhan00 <iron29700@gmail.com>
2025-12-11 02:36:58 +0530

    chech everything

--- deep_learning/tensor-basic.py
+++ deep_learning/tensor-basic.py
@@ -1,8 +1,10 @@
 import torch
 
+# Creating Tensors
 x = torch.tensor([1, 2, 3])        # 1D tensor
 m = torch.tensor([[1, 2], [3, 4]]) # 2D tensor
 
+# Tensor Operations (like NumPy)
 a = torch.tensor([1., 2., 3.])
 b = torch.tensor([4., 5., 6.])
 
@@ -10,6 +12,9 @@ print(a + b)
 print(a * b)
 print(a @ b)   # dot product
 
+# GPU Tensors
 device = "cuda" if torch.cuda.is_available() else "cpu"
 x = torch.tensor([1, 2, 3]).to(device)
 print(x.device)
+
+# Tensor Gradients (Autograd)
